ca7e63ee6ca504c0eb9f5aaab47d69905562c931
Insculpo <30610594+Insculpo@users.noreply.github.com>
2019-03-20 15:42:44 -0700

    more

---
+++ Final_Project/SpaceBoy/Assets/Universe Sounds Free Pack/EyeLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeLook : MonoBehaviour
+{
+    Transform PlayerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPos = FindObjectOfType<ControlShip>().gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 AimDirection = PlayerPos.position - transform.position;
+        Quaternion RottingTurret = Quaternion.LookRotation(AimDirection);
+        RottingTurret *= Quaternion.Euler(90f, 0f, 0f);
+        transform.rotation = RottingTurret;
+    }
+}
